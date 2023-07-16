7f2be9e6dc084e7db5acb51f15977694cf431733
aaron moroney <aaron.createdesign@gmail.com>
2023-07-16 17:55:51 +0100

    problem solving patterns✅

--- algoProblemSolvingPatterns.js
+++ algoProblemSolvingPatterns.js
@@ -52,4 +52,100 @@
     what about empty inputs?
 */
 
+/*
+    ======================
+    22. Break it down
+    ======================
+    get the steps down - write them down w/ comments
+
+    function charCount(string) {
+        //do something
+        //return an obj w/ keys that are lowercase, alphanumeric, numbers
+    }
+    function charCount(string) {
+        //make obj to rerturn @ end
+        //loop over string 
+        -- for each char
+        -- if char is a number/letter & key in obj, add 1 to count 
+        -- if not & if char is a number/letter & key in obj, add it to obj
+        -- if char is something else, ignore.
+        //return obj @ end
+    }
+
+    ======================
+    23. solve or simplify
+    ======================
+
+    solve the problem if you can
+    if you can't, focus on another part of the problem and focus on something else
+
+    simplify
+    ========
+
+    -- 1. find the core difficulty in what you are trying to do
+    -- 2. temporarily ignore that difficulty 
+    -- 3. write a simplified solution
+    -- 4. then incorporate that difficulty back in
+
+    ==========================
+    24. look back and refactor 
+    ==========================
+
+    -- 1. can you check the result
+    -- 2. can you understand at a glance
+    -- 3. can you use result or method for some other problem 
+    -- 4. can you improve the perf. of your solution
+    -- 5. can you think of other ways to refactor?
+    -- 6. how have others soilved this problem?
+*/
+
+let test = 'hello ! world'
+
+function charCount(string) {
+  let result = {};
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i].toLowerCase();
+    if (/[a-z0-9]/.test(char)) {
+      if (result[char] > 0 ) {
+        result[char]++;
+      } else {
+        result[char] = 1;
+      }
+    }
+  }
+  return result; // return the object w/ results of function
+}
+
+console.log(charCount(test));
+
+//refactor - to use more modern and clean syntax (for of)
+function charCountRefact(str) {
+    let obj = {}
+    for (var char of str) {
+        char = char.toLowerCase();
+        if (/[a-z0-9]/.test(char)) {
+            if (obj[char] > 0) {
+                obj[char]++
+            } else {
+                obj[char] = 1;
+            }
+        }
+    }
+    return obj;
+}
+
+//further refactor
+function charCountRefact(str) {
+    let obj = {}
+    for (var char of str) {
+        char = char.toLowerCase();
+        if (/[a-z0-9]/.test(char)) {
+            obj[char] > 0 ? obj[char]++ : obj[char] = 1; 
+        }
+    }
+    return obj;
+}
+  
+  
+
 
