db8980275213a774f0b4fc8caaf00d5794edf12a
aaron moroney <aaron.createdesign@gmail.com>
2023-07-17 20:05:08 +0100

    FNS basic machine in react app ✅

---
+++ FSM.js
@@ -0,0 +1,54 @@
+/* 
+    ============
+    FSM - https://www.youtube.com/watch?v=iDZxjJYMOUQ
+    ============
+
+    the actions your application is in determines the actions that it takes 
+    therefore creating a more predictable & less error prone programme
+*/
+
+//xState - finite state machine react -- in the APP component
+
+import React from 'react';
+import { useMachine } from '@xstate/react';
+import { Machine } from 'xstate';
+import './App.css';
+
+const toggleMachine = new Machine({
+    id: 'toggleMachine', 
+    initial: 'inactive',
+    states: {
+        inactive: {
+            on: {
+                TOGGLE: 'active'
+            }
+        }, 
+        active: {
+            on: {
+                TOGGLE: 'inactive'
+            }
+        }
+    }
+})
+
+function App() {
+    const [ current, send ] = useMachine(toggleMachine)
+    console.log(current);
+    return (
+        <div>
+            <button 
+                onClick={() => {
+                    send('TOGGLE');
+                }}
+            >
+            Toggle
+            </button>
+            {current.matches('active') ? <span> we are active </span> : <span> we are inactive </span>}
+        </div>
+    )
+
+    
+
+}
+
+export default App;
