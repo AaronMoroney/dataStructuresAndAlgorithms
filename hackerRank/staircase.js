3902926c193c9ea6b224ed6261476d56d8f3bef1
aaron moroney <aaron.createdesign@gmail.com>
2023-08-04 19:45:47 +0100

    grading

---
+++ hackerRank/staircase.js
@@ -0,0 +1,16 @@
+function printStaircase(size) {
+    for (let i = 1; i <= size; i++) {
+        const spaces = ' '.repeat(size - i);
+        const hashtags = '#'.repeat(i);
+        console.log(spaces + hashtags);
+    }
+}
+
+const staircaseSize = 6; // Change this to the desired size
+printStaircase(staircaseSize);
+
+/*
+It uses the String.prototype.repeat() method to repeat spaces and hashtags the required number of times.
+Finally, it prints each step using console.log().
+You can change the value of staircaseSize to the desired size of the staircase, and the code will print the staircase pattern with the specified output format.
+*/
