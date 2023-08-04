5e6c155b39981af957897c6c1ede15163a506af8
aaron moroney <aaron.createdesign@gmail.com>
2023-08-04 20:40:33 +0100

    solution

---
+++ hackerRank/rounding.js
@@ -0,0 +1,21 @@
+https://www.hackerrank.com/challenges/grading/problem
+
+
+
+function gradingStudents(grades) {
+    // Write your code here
+    let newGrades = [];
+    for (let i = 0; i < grades.length; i++ ) {
+        //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/ceil
+        let nearestMultiple = Math.ceil(grades[i] / 5) * 5
+        // guard clause for fail
+        if (grades[i] < 38) {
+            newGrades.push(grades[i]);
+        } else if (nearestMultiple - grades[i] < 3) {
+            newGrades.push(nearestMultiple);
+        } else {
+            newGrades.push(grades[i])
+        }  
+    }
+  return(newGrades); 
+}
