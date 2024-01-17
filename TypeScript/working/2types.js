dab13b6d72acfaa87740ae2285de9895e1e30d81
aaron moroney <aaron.createdesign@gmail.com>
2024-01-17 17:18:26 +0000

    inferance, strings & booleans

--- TypeScript/working/2types.js
+++ TypeScript/working/2types.js
@@ -1,7 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+//video 2 - intro
 var greetings = "Aaron";
 var myNum = 6;
 greetings.toLowerCase();
 console.log(greetings);
 console.log(myNum);
+//video 3 - typescript inferance, strings and booleans
+var userId = 1234321.3;
+//video 3 - boolean
+var isLoggedIn = true;
--- TypeScript/working/2types.ts
+++ TypeScript/working/2types.ts
@@ -1,8 +1,18 @@
+//video 2 - intro
 let greetings: string = "Aaron";
 let myNum: number = 6;
 
 greetings.toLowerCase();
 console.log(greetings);
 console.log(myNum);
+
+//video 3 - typescript inferance, strings and booleans
+
+let userId: number = 1234321.3
+
+//video 3 - boolean
+
+let isLoggedIn: boolean = true;
+
 
 export {} 
